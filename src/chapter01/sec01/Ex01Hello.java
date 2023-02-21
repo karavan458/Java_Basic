d530cac9a269c0b868cc99711e10af4bc8b4bfc7
karavan458 <karavan458@gmail.com>
2023-02-22 02:26:28 +0900

    First commit

---
+++ src/chapter01/sec01/Ex01Hello.java
@@ -0,0 +1,7 @@
+package chapter01.sec01;
+
+public class Ex01Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/chapter02/sec01/Ex01VariableInitializationExample.java
@@ -0,0 +1,14 @@
+package sec01;
+
+public class Ex01VariableInitializationExample {
+    public static void main(String[] args) {
+        //변수 value 선언 및 초기화
+        int value = 10;
+
+        //연산 결과를 변수의 초기값으로 대입
+        int result = value + 20;
+
+        //변수 result의 값을 읽고 콘솔에 출력
+        System.out.println(result);
+    }
+}
